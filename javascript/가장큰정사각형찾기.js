22ce19863fcc086e54e67f06956e2b909977ecfc
WHO-A-U <59171080+WHO-A-U@users.noreply.github.com>
2021-01-18 01:39:37 +0900

    LEVEL 2

--- javascript/가장큰정사각형찾기.js
+++ javascript/가장큰정사각형찾기.js
@@ -1,12 +1,11 @@
 function solution(board)
 {
-    let answer = Math.max(board[0][0],board[0][1],board[1][0]);
-    const row = board.length,col = board[0].length;
+    let answer = board[0][0],row = board.length,col = board[0].length;
     for(let i=1;i<row;i++){
         for(let j=1;j<col;j++){
             if(board[i][j]===1)board[i][j]+=Math.min(board[i-1][j-1],board[i-1][j],board[i][j-1]);
             answer = Math.max(answer,board[i][j]); 
         }
     }
-    return answer;
+    return Math.pow(answer,2);
 }
