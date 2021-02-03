2f25a4e3f0b1b60d158b7d5b164789cc268cfb17
WHO-A-U <59171080+WHO-A-U@users.noreply.github.com>
2021-02-04 00:29:40 +0900

    LEVEL 2

---
+++ C++/최솟값만들기.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include<vector>
+#include <algorithm>
+using namespace std;
+
+int solution(vector<int> A, vector<int> B)
+{
+    int answer = 0,n=A.size();
+    sort(A.begin(),A.end());
+    sort(B.begin(),B.end());
+    for(int i=0;i<n;i++)
+        answer+=A[i]*B[n-1-i];
+    return answer;
+}
